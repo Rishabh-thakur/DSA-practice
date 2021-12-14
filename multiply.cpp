4e93a769dae98f85368c52d2537224890059a912
Rishabh Singh <55924975+Rishabh-thakur@users.noreply.github.com>
2021-12-14 22:54:49 +0530

    Editing on second tree

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+       int a,b;
+       cin>>a>>b;
+       cout<<"multiplication of a and b : "<<(a*b)<<endl;
+                          return 0;
+}
